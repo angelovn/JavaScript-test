a5bd4883199f5a3d447f96754ac607fbb995838f
angelovn <dolphinlover36@abv.bg>
2022-10-21 22:12:02 +0300

    good

---
+++ 16.Classes and Attributes - Exercise/tests/12. Payment Package.test.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+const PaymentPackage = require("../12. Payment Package");
+
+describe("paymentPackage Class Test", () => {
+  describe("Construct Parameters Test", () => {
+    let package;
+    beforeEach(() => {
+      package = new PaymentPackage(10, 10);
+    });
+    it("asda", () => {});
+  });
+});
